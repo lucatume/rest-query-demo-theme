9aa16694f8084e9f638737bb4062895ba511bfba
Luca Tumedei <luca@theaveragedev.com>
2016-04-21 18:32:57 +0200

    better rewrites to handle theme customizer paths

--- src/rewrites.php
+++ src/rewrites.php
@@ -9,11 +9,11 @@ add_action( 'generate_rewrite_rules', function () {
 	$includesPath = str_replace( $homeUrl, '', includes_url() );
 
 	$themeRewrites = array(
-		'css/(.*)'          => $themePath . '/css/$1',
-		'js/jquery.js'      => $includesPath . 'js/jquery/jquery.js',
-		'js/underscores.js' => $includesPath . 'js/underscore.min.js',
-		'js/backbone.js'    => $includesPath . 'js/backbone.min.js',
-		'js/(.*)'           => $themePath . '/js/$1',
+		'(wp-admin/)*css/(.*)'          => $themePath . '/css/$2',
+		'(wp-admin/)*js/jquery.js'      => $includesPath . 'js/jquery/jquery.js',
+		'(wp-admin/)*js/underscores.js' => $includesPath . 'js/underscore.min.js',
+		'(wp-admin/)*js/backbone.js'    => $includesPath . 'js/backbone.min.js',
+		'(wp-admin/)*js/(.*)'           => $themePath . '/js/$2',
 	);
 
 	$wp_rewrite->non_wp_rules = array_merge( $wp_rewrite->non_wp_rules, $themeRewrites );
